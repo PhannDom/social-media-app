73745512f78eb61006e4910f28b3b872e32b71a8
TuanPham1604 <tuan.pham@watasolutions.com>
2021-09-29 14:34:04 +0700

    create social media app FE: reactjs BE: nodejs, express

---
+++ tedu_social_react/src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css';
+import './styles/sb-admin-2.min.css';
+
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+
+import { Admin } from './pages/Admin/Admin';
+import { Login } from './pages/Account';
+import { PrivateRoute } from './components';
+import React from 'react';
+
+function App() {
+  return (
+    <div className='App' id='wrapper'>
+      <Router>
+        <Switch>
+          <PrivateRoute>
+            <Login />
+          </PrivateRoute>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
