d85206b3ad75d68d576849f6a35408549fc02e6c
TuanPham1604 <tuan.pham@watasolutions.com>
2021-10-01 10:07:30 +0700

    add refresh token

--- tedu_social_react/src/store/account/types.ts
+++ tedu_social_react/src/store/account/types.ts
@@ -1,6 +1,6 @@
-export const LOGIN_REQUEST = 'LOGIN_REQUEST'
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
-export const LOGIN_FAILURE = 'LOGIN_FAILURE'
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 
 export const LOAD_CURRENT_LOGIN_USER_REQUEST =
   'LOAD_CURRENT_LOGIN_USER_REQUEST';
@@ -9,7 +9,11 @@ export const LOAD_CURRENT_LOGIN_USER_SUCCESS =
 export const LOAD_CURRENT_LOGIN_USER_FAILURE =
   'LOAD_CURRENT_LOGIN_USER_FAILURE';
 
-export const LOG_OUT = 'LOG_OUT'
+export const REFRESH_TOKEN_REQUEST = 'REFRESH_TOKEN_REQUEST';
+export const REFRESH_TOKEN_SUCCESS = 'REFRESH_TOKEN_SUCCESS';
+export const REFRESH_TOKEN_FAILURE = 'REFRESH_TOKEN_FAILURE';
+
+export const LOG_OUT = 'LOG_OUT';
 
 export interface AuthenticatedUser {
   _id: string;
@@ -31,6 +35,7 @@ interface LoginSuccess {
   type: typeof LOGIN_SUCCESS;
   payload: {
     token: string;
+    refreshToken: string;
   };
 }
 
@@ -63,11 +68,30 @@ interface LoadCurrentLoginUserFailure {
   };
 }
 
+interface RefreshTokenRequest {
+  type: typeof REFRESH_TOKEN_REQUEST;
+}
+
+interface RefreshTokenSuccess {
+  type: typeof REFRESH_TOKEN_SUCCESS;
+  payload: {
+    token: string;
+    refreshToken: string;
+  };
+}
+
+interface RefreshTokenFailure {
+  type: typeof REFRESH_TOKEN_FAILURE;
+  payload: {
+    error: string;
+  };
+}
 export interface AccountState {
   user: AuthenticatedUser | null;
   loading: boolean;
   error: string | null;
   token: string | null;
+  refreshToken: string | null;
 }
 
 export type AccountActionTypes =
@@ -77,4 +101,7 @@ export type AccountActionTypes =
   | Logout
   | LoadCurrentLoginUserRequest
   | LoadCurrentLoginUserSuccess
-  | LoadCurrentLoginUserFailure;
+  | LoadCurrentLoginUserFailure
+  | RefreshTokenRequest
+  | RefreshTokenSuccess
+  | RefreshTokenFailure;
